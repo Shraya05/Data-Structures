d0c20df55719dd26f41385e9d5d7806a4db486d3
Shraya <shraya978@gmail.com>
2024-12-10 23:17:31 +0530

    Updated stack implementation

---
+++ DataStructures/src/stack/ArrayStack.java
@@ -0,0 +1,35 @@
+package stack;
+
+public class ArrayStack<T>{
+
+	static final int MAX_SIZE=30;
+	T arr[]=(T[])new Object[MAX_SIZE];
+	int top;
+	public ArrayStack()
+	{
+		top=-1;
+	}
+	void push(T val)
+	{
+		if(top==MAX_SIZE-1)
+			throw new IndexOutOfBoundsException("Stack Overflow");
+		arr[++top]=val;
+	}
+	
+	T pop()
+	{
+		if(top==-1)
+			throw new IndexOutOfBoundsException("Stack underflow");
+		return arr[top--];
+	}
+	
+	T peek()
+	{
+		return arr[top];
+	}
+	
+	boolean isEmpty()
+	{
+		return top==-1;
+	}
+}
---
+++ DataStructures/src/stack/LinkedStack.java
@@ -0,0 +1,41 @@
+package stack;
+
+public class LinkedStack<T> {
+ class Node{
+	 T data;
+		Node next;
+		Node(T val){
+			data = val;
+			next = null;
+		}
+	}
+	
+	Node top;
+	LinkedStack(){
+		top = null;
+	}
+	
+	public void push(T val) {
+		Node newNode = new Node(val);
+		newNode.next = top;
+		top = newNode;
+	}
+	
+	public T pop() {
+		if(top==null)
+			throw new IndexOutOfBoundsException("Stack is Empty");
+		
+		T tmp = top.data;
+		top = top.next;
+		return tmp;
+		
+	}
+	
+	public boolean isEmpty() {
+		return top==null;
+	}
+	
+	public T peek() {
+		return top.data;
+ }
+}
