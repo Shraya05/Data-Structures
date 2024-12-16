e5539953fa9afe0f3b60470b6c35f1751e300212
Shraya <shraya978@gmail.com>
2024-12-16 23:25:51 +0530

    Implementation of Linked list added

---
+++ DataStructures/src/LLUtil.java
@@ -0,0 +1,19 @@
+
+public class LLUtil {
+public static void main(String[] args)
+{
+	 LinkedList list = new LinkedList();
+
+     list.insertAtBeginning(10);
+     list.insertAtBeginning(20);
+     list.print(); 
+     list.insertAtEnd(30);
+     list.print(); 
+     list.insertAtposition(25, 2);
+     list.print(); 
+     list.insertAtposition(40, 10); 
+     list.print(); 
+     list.insertAtposition(5, 0);
+     list.print(); 
+}
+}
