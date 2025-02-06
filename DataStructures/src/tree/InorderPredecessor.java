7a027f66729c4542c76755b91fa53a727ee98113
Shraya <shraya978@gmail.com>
2025-02-07 00:55:18 +0530

    Iterative Traversals

--- DataStructures/src/tree/InorderPredecessor.java
+++ DataStructures/src/tree/InorderPredecessor.java
@@ -1,5 +1,31 @@
 package tree;
-
+import java.util.*;
 public class InorderPredecessor {
+	public static void Inorder(Node root,List<Integer> res)
+    {
+        
+        if(root==null) return;
+        Inorder(root.left,res);
+        res.add(root.data);
+        Inorder(root.right,res);
+    }
+public static void find(Node root,int val )
+{
+    List<Integer> li=new ArrayList<>();
+    Inorder(root,li);
+    for(int i=0;i<li.size();i++)
+    {
+        if(li.get(i)== val)
+        {
+            if(i>0) System.out.print(li.get(i-1));
+        }
+    }
+    
+}
+public static void main(String[] args) {
+	Integer[] arr={1,2,3,4,5,6};
+	Node node=insert(arr);
+   find(node,3);
+}
 
 }
--- DataStructures/src/tree/LevelOrderInsertion.java
+++ DataStructures/src/tree/LevelOrderInsertion.java
@@ -1,10 +1,63 @@
 package tree;
-
+import java.util.*;
+class Node{
+    int data;
+    Node left,right;
+    Node(int val)
+    {
+        data=val;
+        left=right=null;
+    }
+}
 public class LevelOrderInsertion {
-
+	public static Node insert(Integer[] arr)
+    {
+        if(arr.length==0 && arr[0]==null) return null;
+        
+        Node root=new Node(arr[0]);
+        Queue<Node> q=new LinkedList<>();
+        q.add(root);
+        int i=1;
+        while(i<arr.length && !q.isEmpty())
+        {
+            Node curr=q.poll();
+            if(arr[i]!=null)
+            {
+                curr.left=new Node(arr[i]);
+                q.add(curr.left);
+            }
+            i++;
+            if(i<arr.length && arr[i]!=null)
+            {
+                curr.right=new Node(arr[i]);
+                q.add(curr.right);
+            }
+            i++;
+        }
+        return root;
+    }
+    public static void print(Node root)
+    {
+        Queue<Node> q=new LinkedList<>();
+        q.add(root);
+        while(!q.isEmpty())
+        {
+            Node curr=q.poll();
+            System.out.print(curr.data);
+            if(curr.left!=null)
+            {
+                q.add(curr.left);
+            }
+            if(curr.right!=null)
+            {
+                q.add(curr.right);
+            }
+        }
+    }
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Integer[] arr={1,2,3,null,5,6};
+		Node node=insert(arr);
+		print(node);
 	}
 
 }
