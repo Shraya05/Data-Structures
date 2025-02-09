35c96c1db833dabbe5af031f24cfdcd91f9c2aa9
Shraya <shraya978@gmail.com>
2025-02-09 22:45:20 +0530

    using xor method

--- DataStructures/src/tree/Iterative_Inorder.java
+++ DataStructures/src/tree/Iterative_Inorder.java
@@ -1,5 +1,29 @@
 package tree;
 
-public class Iterative_Inorder {
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
 
+public class Iterative_Inorder {
+	public static List<Integer> inorder(Node node)
+	{
+List<Integer> li=new ArrayList<>();
+Stack<Node> st=new Stack<>();
+while(true)
+{
+	if(node!=null)
+	{
+		st.push(node);
+		node=node.left;
+	}
+	else
+	{
+		if(st.isEmpty()) break;
+		node=st.pop();
+		li.add(node.data);
+		node=node.right;
+	}
+}
+return li;
+	}
 }
--- DataStructures/src/tree/Iterative_Preorder.java
+++ DataStructures/src/tree/Iterative_Preorder.java
@@ -1,5 +1,23 @@
 package tree;
 
-public class Iterative_Preorder {
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
 
+public class Iterative_Preorder {
+public static List<Integer> preorder(Node root)
+{
+	List<Integer> li = new ArrayList<>();
+	Stack<Node> st=new Stack<>();
+	if (root==null) return li;
+	st.push(root);
+	while(!st.isEmpty())
+	{
+		Node curr=st.pop();
+		li.add(curr.data);
+		if(curr.right!=null) st.push(curr.right);
+		if(curr.left!=null) st.push(curr.left);
+	}
+	return li;
+}
 }
